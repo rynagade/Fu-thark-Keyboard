58989f2351c4b97232015922dc524b2c5680ae89
Anthony <jarius327@icloud.com>
2023-10-28 12:02:39 -0500

    Initial Commit

---
+++ Fúthark Keyboard/Fu_thark_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fu_thark_KeyboardApp.swift
+//  Fúthark Keyboard
+//
+//  Created by Rynagade on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Fu_thark_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
